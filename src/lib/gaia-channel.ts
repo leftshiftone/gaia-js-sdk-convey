c5dde78ee0e55d5074d321212b14c51498396fa2
Michael Mair <michael.mair@leftshift.one>
2018-11-22 20:37:08 +0100

    adapted mqtt messages

--- src/lib/gaia-channel.ts
+++ src/lib/gaia-channel.ts
@@ -1,23 +1,34 @@
-import {Emitter, EmitterAdapter} from './emitter';
-import {Renderer} from './renderer';
+import { Emitter, EmitterAdapter } from './emitter';
+import { Renderer } from './renderer';
 import * as mqtt from 'mqtt';
+import { Packet } from 'mqtt';
 
 export class GaiaChannel {
 
     private emitter: EmitterAdapter;
     private renderer: Renderer;
     private mqttClient?: mqtt.MqttClient;
+    private _clientId: string;
+    private _identityId: string;
+    private _userId: number;
+    private _inboundDestination: string;
+    private _outboundDestination: string;
 
-    constructor(container: any, emitter: Emitter | null) {
+    constructor(container: any, identityId: string, emitter: Emitter | null) {
         this.emitter = new EmitterAdapter(emitter);
         this.renderer = new Renderer(document.querySelector(container));
+        this._clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8);
+        this._identityId = identityId;
+        this._userId = Math.random(); //todo: make user id more persistent for an actual user (e.g. cookie, etc)
+        this._inboundDestination = 'GAIA/RAIN/' + this._clientId + '/' + this._identityId + '/in';
+        this._outboundDestination = 'GAIA/RAIN/' + this._clientId + '/' + this._identityId + '/out';
         this.onMessage = this.onMessage.bind(this);
     }
 
     public connect(url: any) {
         if (!this.mqttClient) {
             return new Promise((resolve) => {
-                this.mqttClient = mqtt.connect(url, { clean: false, clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8) });
+                this.mqttClient = mqtt.connect(url, { clean: false, clientId: this._clientId });
                 this.mqttClient.on('connect', this.emitter.onConnected);
                 this.mqttClient.on('message', this.onMessage);
                 resolve();
@@ -56,19 +67,42 @@ export class GaiaChannel {
             const elements = document.querySelectorAll('button.left');
             elements.forEach(element => element.remove());
 
-            const message = Object.assign(msg, { position: 'right', timestamp: new Date().getTime() });
+            const header = { userId: this.userId };
+            const body = Object.assign(msg, { position: 'right', timestamp: new Date().getTime() });
             if (this.mqttClient) {
-                this.mqttClient.publish(topic, JSON.stringify(message));
+                this.mqttClient.publish(topic, JSON.stringify({ header, body }), (error?: Error, packet?: Packet) => {
+                    if (error) {
+                        console.error('Failed to publish message ' + error.message, error, packet);
+                    } else {
+                        console.debug('Successfully published message');
+                    }
+                });
             }
 
             const $this = this;
-            return this.emitter.onPreRender(message, true).then(msg => {
+            return this.emitter.onPreRender(body, true).then(msg => {
                 $this.renderer.render(msg, $this.sendMessage.bind($this, topic));
                 return this.emitter.onPostRender(msg, true);
-            },                                                  console.error);
+            },                                               console.error);
         } catch (err) {
             return Promise.reject(err);
         }
+    }
+
+    get idenityId(): string {
+        return this._identityId;
+    }
+
+    get userId(): number {
+        return this._userId;
+    }
+
+    get inboundDestination(): string {
+        return this._inboundDestination;
+    }
+
+    get outboundDestination(): string {
+        return this._outboundDestination;
     }
 
     private onMessage(topic: string, msg: string) {
@@ -87,5 +121,4 @@ export class GaiaChannel {
                 });
         }
     }
-
 }
