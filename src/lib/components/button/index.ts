4d9cd7fb85d0c4a3a1ba71c67c90b25fa85a4641
Michael Mair <michael.mair@leftshift.one>
2018-11-27 15:09:29 +0100

    send explicit attribute field for button

--- src/lib/components/button/index.ts
+++ src/lib/components/button/index.ts
@@ -1,43 +1,42 @@
-import { MarkupComponent } from '../markup-component';
+import {MarkupComponent} from '../markup-component';
 
 export class Button extends MarkupComponent {
 
-  public text: string;
-  public buttonName: string;
-  public value: string;
-  public position: string;
+    public text: string;
+    public buttonName: string;
+    public value: string;
+    public position: string;
 
-  constructor(message: any) {
-    super('button');
-    this.text = message.text;
-    this.buttonName = message.name;
-    this.value = message.value;
-    this.position = message.position;
-  }
-
-  public render(container: any, sendMessage: any) {
-    console.debug('Send message function: ');
-    console.debug(sendMessage);
-
-    const position = this.position || 'left';
-    const button = document.createElement('button');
-    button.setAttribute('name', this.buttonName);
-
-    if (!Button.isNested(container)) {
-      button.classList.add('button', position);
-    } else {
-      button.classList.add('button-nested', position);
+    constructor(message: any) {
+        super('button');
+        this.text = message.text;
+        this.buttonName = message.name;
+        this.value = message.value;
+        this.position = message.position;
     }
-    button.appendChild(document.createTextNode(this.text));
-    container.appendChild(button);
-    button.addEventListener('click', () => {
-      sendMessage({
-        type: 'button',
-        text: this.text,
-        name: this.buttonName,
-        value: this.value,
-      },
+
+    public render(container: any, sendMessage: any) {
+        console.debug('Send message function: ');
+        console.debug(sendMessage);
+
+        const position = this.position || 'left';
+        const button = document.createElement('button');
+        button.setAttribute('name', this.buttonName);
+
+        if (!Button.isNested(container)) {
+            button.classList.add('button', position);
+        } else {
+            button.classList.add('button-nested', position);
+        }
+        button.appendChild(document.createTextNode(this.text));
+        container.appendChild(button);
+        button.addEventListener('click', () => {
+            sendMessage({
+                    type: 'button',
+                    text: this.text,
+                    attributes: {name: this.buttonName, value: this.value}
+                },
             );
-    });
-  }
+        });
+    }
 }
