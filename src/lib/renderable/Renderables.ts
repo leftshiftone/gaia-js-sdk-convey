73ad76b77bd2ce46e5356bdbe84b19f5d92f725a
christian <christian.weber@leftshift.one>
2019-01-28 15:28:06 +0100

    class Renderables refactored

--- src/lib/renderable/Renderables.ts
+++ src/lib/renderable/Renderables.ts
@@ -6,31 +6,8 @@ export default class Renderables {
         this.renderableMap[name.toUpperCase()] = renderable;
     }
 
-export const renderables: object = {
-    BREAK: Break,
-    BLOCK: Block,
-    BOLD: Bold,
-    BUTTON: Button,
-    CHECKBOX: Checkbox,
-    COL: Col,
-    HEADLINE: Headline,
-    IMAGE: Image,
-    ITEM: Item,
-    ITEMS: Items,
-    LINK: Link,
-    MAP: Map,
-    ROW: Row,
-    SUBMIT: Submit,
-    TABLE: Table,
-    TEXT: Text,
-    CAROUSEL: Carousel,
-    CONTAINER: Container,
-    SUGGESTION: Suggestion,
-    SLIDER: Slider,
-    SPINNER: Spinner,
-    CALENDAR: Calendar,
-    SLOTMACHINE: SlotMachine,
-    REEL: Reel,
-    DATEPICKER: DatePicker,
-    DATETIMEPICKER: DateTimePicker,
-};
+    public static resolve(name: string) {
+        return this.renderableMap[name.toUpperCase()];
+    }
+
+}
