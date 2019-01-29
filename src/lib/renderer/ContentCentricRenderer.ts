4665341d3721c66d115692274567f0dedb38e33a
christian <christian.weber@leftshift.one>
2019-01-29 20:41:34 +0100

    ContentCentricRenderer modified

--- src/lib/renderer/ContentCentricRenderer.ts
+++ src/lib/renderer/ContentCentricRenderer.ts
@@ -4,6 +4,7 @@ import EventStream from '../event/EventStream';
 import {Overlay} from '../renderable/overlay';
 import {IStackeable} from '../api/IStackeable';
 import {Block} from '../renderable/block';
+import {Container} from '../renderable/container';
 
 /**
  * The content centric renderer tries to maximize the time a content is visible by updating
@@ -12,7 +13,7 @@ import {Block} from '../renderable/block';
 export class ContentCentricRenderer extends ClassicRenderer {
 
     private qualifier = null;
-    private behaviour:(renderable:IRenderable, type?:IStackeable) => HTMLElement[] = (r, t) => this.defaultBehaviour(r, t);
+    private behaviour: (renderable: IRenderable, type?: IStackeable) => HTMLElement[] = (r, t) => this.defaultBehaviour(r, t);
 
     constructor(container: HTMLElement) {
         super(container);
@@ -31,15 +32,15 @@ export class ContentCentricRenderer extends ClassicRenderer {
         return (renderable["spec"] !== undefined) ? renderable["spec"].qualifier : null;
     }
 
-    private defaultBehaviour(renderable:IRenderable, containerType?:IStackeable) {
+    private defaultBehaviour(renderable: IRenderable, containerType?: IStackeable) {
         if (!containerType) {
             this.qualifier = ContentCentricRenderer.getQualifier(renderable) || this.qualifier;
         }
         return super.renderElement(renderable, containerType);
     }
 
-    private suggestionBehaviour(qualifier?:string) {
-        return (renderable:IRenderable, containerType?:IStackeable) => {
+    private suggestionBehaviour(qualifier?: string) {
+        return (renderable: IRenderable, containerType?: IStackeable) => {
             if (ContentCentricRenderer.getQualifier(renderable) === qualifier) {
                 const containers = document.getElementsByClassName("lto-container");
                 const containerZ = containers[containers.length - 1];
@@ -51,10 +52,10 @@ export class ContentCentricRenderer extends ClassicRenderer {
                 this.behaviour = (r, t) => this.defaultBehaviour(r, t);
                 return super.renderElement(renderable, containerType);
             }
-            if (!containerType) {
-                return super.renderElement(new Overlay(renderable), new Block({type:"block"}));
+            if (renderable instanceof Container) {
+                return super.renderElement(new Overlay(renderable), new Block({type: "block"}));
             }
-            return super.renderElement(renderable, new Block({type:"block"}));
+            return super.renderElement(renderable, new Block({type: "block"}));
         };
     }
 
