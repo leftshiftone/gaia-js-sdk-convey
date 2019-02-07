6dbcd7f2162f5366b1e78983b7f0e91b6cfaf0e2
Patrick Arbeiter <patrick.arbeiter@leftshift.one>
2019-02-07 16:03:17 +0100

    set timeout to google map

--- src/lib/renderable/map/google/index.ts
+++ src/lib/renderable/map/google/index.ts
@@ -33,110 +33,114 @@ export class GoogleMap {
 
         GoogleMapsLoader.LANGUAGE = "de";
         GoogleMapsLoader.KEY = "AIzaSyDBWbYLJkXygR90IoTTjbFOC832thRCAyQ";
-        GoogleMapsLoader.load((google: GoogleMapsLoader.google) => {
-            const map = new google.maps.Map(document.getElementsByClassName("lto-map").item(0), {
-                center: this.center,
-                zoom: 8,
-                minZoom: 7,
-                maxZoom: 12,
-                streetViewControl: false
-            });
 
-            const ICON_ACTIVE = {
-                url: 'http://www.myiconfinder.com/uploads/iconsets/256-256-a5485b563efc4511e0cd8bd04ad0fe9e.png',
-                scaledSize: new google.maps.Size(30,30)
-            };
-            const ICON_INACTIVE = {
-                url: "https://cdn2.iconfinder.com/data/icons/basic-ict-line-icons/100/30-512.png",
-                scaledSize: new google.maps.Size(30,30)
-            };
-            const ICON_SELECTED = {
-                url: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png',
-                scaledSize: new google.maps.Size(30,30)
-            };
-
-            const circle = new google.maps.Circle({
-                strokeColor: '#FF0000',
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: '#FF0000',
-                fillOpacity: 0.35,
-                map: map,
-                radius: 10000
-            });
-
-            if (this.markers !== undefined) {
-
-                const _markers: Array<[google.maps.Marker, boolean]> = [];
-
-                this.markers.forEach((marker: IMarker) => {
-                    const m = new google.maps.Marker({
-                        position: marker[0].position,
-                        map: map,
-                        icon: marker[0].active ? ICON_ACTIVE : ICON_INACTIVE,
-                    });
-
-                    m.set("meta", marker[0].meta);
-                    _markers.push([m, marker[0].active])
+        setTimeout(() => {
+            GoogleMapsLoader.load((google: GoogleMapsLoader.google) => {
+                const map = new google.maps.Map(document.getElementsByClassName("lto-map").item(0), {
+                    center: this.center,
+                    zoom: 8,
+                    minZoom: 7,
+                    maxZoom: 12,
+                    streetViewControl: false
                 });
 
-                if(this.spec.exact) {
-                    let selectedMarker: google.maps.Marker;
-                    _markers.forEach(marker => {
-                        if(marker[1]) { // is active?
-                            marker[0].addListener("click", () => {
-                                if(selectedMarker !== undefined) { selectedMarker.setIcon(ICON_ACTIVE) }
-                                setValueToContainer({position: marker[0].getPosition(), meta: marker[0].get("meta")});
-                                marker[0].setIcon(ICON_SELECTED);
-                                selectedMarker = marker[0];
-                            })
-                        }
-                    })
-                } else {
-                    map.addListener("center_changed", () => {
-                        drawCircle();
-                        const markersInCircle: {markers: any} = {markers: []};
-                        _markers.forEach(marker => {
-                            if(marker[1]) {
-                                if(circle.getBounds().contains(marker[0].getPosition())) {
-                                    markersInCircle.markers.push({
-                                        position: marker[0].getPosition(),
-                                        meta: marker[0].get("meta")
-                                    });
-                                    marker[0].setIcon(ICON_SELECTED);
-                                } else {
-                                    marker[0].setIcon(ICON_ACTIVE);
-                                }
-                            }
+                const ICON_ACTIVE = {
+                    url: 'http://www.myiconfinder.com/uploads/iconsets/256-256-a5485b563efc4511e0cd8bd04ad0fe9e.png',
+                    scaledSize: new google.maps.Size(30,30)
+                };
+                const ICON_INACTIVE = {
+                    url: "https://cdn2.iconfinder.com/data/icons/basic-ict-line-icons/100/30-512.png",
+                    scaledSize: new google.maps.Size(30,30)
+                };
+                const ICON_SELECTED = {
+                    url: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png',
+                    scaledSize: new google.maps.Size(30,30)
+                };
+
+                const circle = new google.maps.Circle({
+                    strokeColor: '#FF0000',
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    fillColor: '#FF0000',
+                    fillOpacity: 0.35,
+                    map: map,
+                    radius: 10000
+                });
+
+                if (this.markers !== undefined) {
+
+                    const _markers: Array<[google.maps.Marker, boolean]> = [];
+
+                    this.markers.forEach((marker: IMarker) => {
+                        const m = new google.maps.Marker({
+                            position: marker[0].position,
+                            map: map,
+                            icon: marker[0].active ? ICON_ACTIVE : ICON_INACTIVE,
                         });
-                        setValueToContainer(markersInCircle);
+
+                        m.set("meta", marker[0].meta);
+                        _markers.push([m, marker[0].active])
                     });
-                }
-            } else {
-                if(this.spec.exact) {
-                    const marker = new google.maps.Marker({position: {lat: 0, lng: 0}});
-                    map.addListener("click", ev => {
-                        marker.setMap(map);
-                        marker.setPosition(ev.latLng);
-                        setValueToContainer({position: marker.getPosition()})
-                    })
+
+                    if(this.spec.exact) {
+                        let selectedMarker: google.maps.Marker;
+                        _markers.forEach(marker => {
+                            if(marker[1]) { // is active?
+                                marker[0].addListener("click", () => {
+                                    if(selectedMarker !== undefined) { selectedMarker.setIcon(ICON_ACTIVE) }
+                                    setValueToContainer({position: marker[0].getPosition(), meta: marker[0].get("meta")});
+                                    marker[0].setIcon(ICON_SELECTED);
+                                    selectedMarker = marker[0];
+                                })
+                            }
+                        })
+                    } else {
+                        map.addListener("center_changed", () => {
+                            drawCircle();
+                            const markersInCircle: {markers: any} = {markers: []};
+                            _markers.forEach(marker => {
+                                if(marker[1]) {
+                                    if(circle.getBounds().contains(marker[0].getPosition())) {
+                                        markersInCircle.markers.push({
+                                            position: marker[0].getPosition(),
+                                            meta: marker[0].get("meta")
+                                        });
+                                        marker[0].setIcon(ICON_SELECTED);
+                                    } else {
+                                        marker[0].setIcon(ICON_ACTIVE);
+                                    }
+                                }
+                            });
+                            setValueToContainer(markersInCircle);
+                        });
+                    }
                 } else {
-                    map.addListener("center_changed", () => {
-                        drawCircle();
-                        setValueToContainer({position: circle.getCenter()})
-                    })
+                    if(this.spec.exact) {
+                        const marker = new google.maps.Marker({position: {lat: 0, lng: 0}});
+                        map.addListener("click", ev => {
+                            marker.setMap(map);
+                            marker.setPosition(ev.latLng);
+                            setValueToContainer({position: marker.getPosition()})
+                        })
+                    } else {
+                        map.addListener("center_changed", () => {
+                            drawCircle();
+                            setValueToContainer({position: circle.getCenter()})
+                        })
+                    }
                 }
-            }
 
-            function setValueToContainer(value: object) {
-                container.setAttribute("value", JSON.stringify(value));
-            }
+                function setValueToContainer(value: object) {
+                    container.setAttribute("value", JSON.stringify(value));
+                }
 
-            function drawCircle() {
-                circle.setCenter(map.getCenter());
-                circle.setMap(map);
-            }
-        });
+                function drawCircle() {
+                    circle.setCenter(map.getCenter());
+                    circle.setMap(map);
+                }
+            });
+        }, 500);
+
         return container;
     }
 
