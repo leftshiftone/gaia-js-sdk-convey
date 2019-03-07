8a879618dd34a3e69e813228f3671ae8e768fbe2
Patrick Arbeiter <patrick.arbeiter@leftshift.one>
2019-03-07 09:29:04 +0100

    added labels for min and max value to slider

--- src/lib/renderable/slider/index.ts
+++ src/lib/renderable/slider/index.ts
@@ -19,6 +19,9 @@ export class Slider implements IRenderable {
     public render(renderer: IRenderer, isNested: boolean): HTMLElement {
         const position = this.spec.position || 'left';
         const slider = document.createElement("input");
+        const minLabel = document.createElement("span");
+        const maxLabel = document.createElement("span");
+
         slider.type="range";
         //@ts-ignore
         slider.value = isNaN(this.spec.value) ? "" : this.spec.value;
@@ -29,10 +32,16 @@ export class Slider implements IRenderable {
         slider.step = this.spec.step || "";
         slider.name = this.spec.name || "";
 
+        minLabel.classList.add("lto-slider-min");
+        maxLabel.classList.add("lto-slider-max");
+        minLabel.innerText = slider.min;
+        maxLabel.innerText = slider.max;
+
         const value = document.createElement("div");
         value.classList.add("lto-slider-value");
 
         value.innerHTML = slider.value;
+        slider.setAttribute('value', slider.value);
 
         slider.oninput = () => {
             slider.setAttribute('value', slider.value);
@@ -41,24 +50,25 @@ export class Slider implements IRenderable {
 
         slider.classList.add("lto-slider", "lto-" + position);
 
-        if (!this.spec.horizontal) {
+        if (!this.spec.horizontal)
             slider.style.transform = "rotate(90deg)";
-        }
 
         slider.step = this.spec.step || "";
 
-        if (isNested) { slider.classList.add("lto-nested") }
+        if (isNested)
+            slider.classList.add("lto-nested");
 
         const container = document.createElement("div");
         container.classList.add("lto-slider-container");
-        if(this.spec.class !== undefined) {
+        if(this.spec.class !== undefined)
             this.spec.class.split(" ").forEach(e => container.classList.add(e));
-        }
 
         slider.appendChild(document.createTextNode(this.spec.text || ""));
 
         container.appendChild(value);
+        container.appendChild(minLabel);
         container.appendChild(slider);
+        container.appendChild(maxLabel);
 
         return container;
     }
