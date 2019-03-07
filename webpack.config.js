7bb580be40b66b915bc4b7bd664f868283971698
Patrick Arbeiter <patrick.arbeiter@leftshift.one>
2019-03-07 12:25:45 +0100

    modified package.json & webpack-config

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,46 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = (env, argv) => ({
-  entry: {
-    'gaia-js-sdk-convey-std': './src/std.ts',
-    'gaia-js-sdk-convey-all': './src/all.ts',
-    'gaia-js-sdk-convey-cal': './src/cal.ts',
-    'gaia-js-sdk-convey-map': './src/map.ts',
-    'gaia-js-sdk-convey-vis': './src/vis.ts',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(css|scss)$/,
-        exclude: /node_modules/,
-        use: [
-          argv.mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
-          {
-            loader: 'css-loader',
-          },
-          {
-            loader: 'sass-loader',
-          },
-          'import-glob-loader',
+    entry: {
+        'gaia-js-sdk-convey-std': './src/std.ts',
+        'gaia-js-sdk-convey-all': './src/all.ts',
+        'gaia-js-sdk-convey-cal': './src/cal.ts',
+        'gaia-js-sdk-convey-map': './src/map.ts',
+        'gaia-js-sdk-convey-vis': './src/vis.ts',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(css|scss)$/,
+                exclude: /node_modules/,
+                use: [
+                    argv.mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                    'import-glob-loader',
+                ],
+            },
         ],
-      },
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        library: '',
+        libraryTarget: 'commonjs',
+        filename: 'dist/[name].min.js',
+        path: __dirname,
+    },
+    externals: [nodeExternals()],
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'dist/[name].min.css',
+        }),
     ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  output: {
-    library: '',
-    libraryTarget: 'commonjs',
-    filename: 'dist/[name].min.js',
-    path: __dirname,
-  },
-  externals: ['google-maps', 'leaflet', 'node-ical'],
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'dist/[name].min.css',
-    }),
-  ],
 });
