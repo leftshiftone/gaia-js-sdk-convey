8d79cca229af4da23f749f3de375a6ca2c5558c6
Patrick Arbeiter <patrick.arbeiter@leftshift.one>
2019-05-16 11:39:11 +0200

    added labels to the upload component for: file size, file extension, error when extension is not valid, error error when size is not valid

--- src/lib/renderable/upload/index.ts
+++ src/lib/renderable/upload/index.ts
@@ -8,6 +8,9 @@ import Renderables from '../Renderables';
 export class Upload implements IRenderable {
 
     private readonly spec: ISpecification;
+    private errorSpan = document.createElement("span");
+    private fileSpan = document.createElement("span");
+    private dropArea = document.createElement("div");
 
     constructor(spec: ISpecification) {
         this.spec = spec;
@@ -19,99 +22,88 @@ export class Upload implements IRenderable {
     public render(renderer: IRenderer, isNested: boolean): HTMLElement {
         const position = this.spec.position || 'left';
 
-        const dropArea = document.createElement("div");
         const upload = document.createElement("input");
         const uploadLabel = document.createElement("label");
         const textSpan = document.createElement("span");
-        const fileSpan = document.createElement("span");
-        const errorSpan = document.createElement("span");
+        const extensionSpan = document.createElement("span");
+        const sizeSpan = document.createElement("span");
 
+        this.errorSpan.className = "lto-upload-error-label";
+        this.fileSpan.className = "lto-upload-file-label";
+        extensionSpan.className = "lto-upload-extension-label";
+        sizeSpan.className = "lto-upload-size-label";
+        textSpan.className = "lto-upload-text-label";
+        uploadLabel.className = "lto-upload";
         textSpan.innerText = this.spec.text || "";
-        errorSpan.classList.add("lto-upload-error-label");
-        textSpan.classList.add("lto-upload-text-label");
-        fileSpan.classList.add("lto-upload-file-label");
-        uploadLabel.classList.add("lto-upload", "lto-" + position);
+        extensionSpan.innerText = this.spec.accept || "";
+        sizeSpan.innerText = this.spec.maxSize!.toString() || "";
         upload.type = "file";
-        upload.accept = this.spec.accept || "";
-        dropArea.classList.add("lto-drop-area", "lto-" + position);
-        dropArea.setAttribute("name", this.spec.name || "");
+
+        if(this.spec.accept) {
+            const acceptArray = this.spec.accept.replace(/\s/g, '').split(",");
+            upload.accept = "."+acceptArray.join(",.");
+        }
+
+        this.dropArea.classList.add("lto-drop-area", "lto-" + position);
+        this.dropArea.setAttribute("name", this.spec.name || "");
 
         if (isNested) {
             uploadLabel.classList.add("lto-nested");
-            dropArea.classList.add("lto-nested");
+            this.dropArea.classList.add("lto-nested");
         }
 
-        if (this.spec.class !== undefined) {
+        if (this.spec.class !== undefined)
             this.spec.class.split(" ").forEach(e => {
                 uploadLabel.classList.add(e);
-                dropArea.classList.add(e);
+                this.dropArea.classList.add(e);
             });
-        }
-        dropArea.ondragover = (ev) => ev.preventDefault();
 
-        dropArea.ondrop = (ev) => {
+        this.dropArea.ondragover = (ev) => ev.preventDefault();
+
+        this.dropArea.ondrop = (ev) => {
             ev.preventDefault();
-
-            // @ts-ignore
-            const file = ev.dataTransfer.items ? ev.dataTransfer.items[0].getAsFile() : ev.dataTransfer.files[0];
-
-            if (file) {
-                const fileExtension = file.name.split(".")[1];
-                let extensionAllowed = false;
-                if (this.spec.accept !== undefined) {
-                    const allowedExtensions = this.spec.accept!.replace(/\s/g, '').split(",");
-                    allowedExtensions.forEach(e => {
-                        if (e === "." + fileExtension) extensionAllowed = true;
-                    });
-                } else extensionAllowed = true;
-
-                if (extensionAllowed && file.size <= this.spec.maxSize! * 1024 * 1024) {
-                    fileSpan.innerText = file.name;
-                    errorSpan.innerText = "";
-                    this.getBase64(file)
-                        .then(data => {
-                            dropArea.setAttribute("value",JSON.stringify({
-                                data: data.toString().split(",")[1],
-                                fileExtension: fileExtension,
-                                fileName: file.name,
-                                mimeType: data.toString().split(";")[0].split(":")[1]
-                            }))
-                        })
-                        .catch(reason => console.error("ERROR: " + reason));
-                } else {
-                    errorSpan.innerText = "file not valid";
-                    fileSpan.innerText = "";
-                }
+            let file = null;
+            if (ev.dataTransfer) {
+                file = ev.dataTransfer.items ? ev.dataTransfer.items[0].getAsFile() : ev.dataTransfer.files[0];
             }
+
+            if (file !== null)
+                this.doValidateAndGetBase64(file);
         };
 
         upload.onchange = () => {
-            if (upload.files) {
-                if (upload.files[0].size <= this.spec.maxSize! * 1024 * 1024) {
-                    fileSpan.innerText = upload.files[0].name;
-                    errorSpan.innerText = "";
-                    this.getBase64(upload.files[0])
-                        .then(data => dropArea.setAttribute("value", JSON.stringify({
-                            data: data.toString().split(",")[1],
-                            fileExtension: upload.files![0].name.split(".")[1],
-                            fileName: upload.files![0].name,
-                            mimeType: data.toString().split(";")[0].split(":")[1]
-                        })))
-                        .catch(reason => console.error("ERROR: " + reason));
-                } else {
-                    errorSpan.innerText = "file not valid";
-                    fileSpan.innerText = "";
-                }
+            if (upload.files && upload.files[0]) {
+                this.doValidateAndGetBase64(upload.files[0]);
             }
         };
 
         uploadLabel.appendChild(upload);
         uploadLabel.appendChild(textSpan);
-        dropArea.appendChild(uploadLabel);
-        dropArea.appendChild(fileSpan);
-        dropArea.appendChild(errorSpan);
+        this.dropArea.appendChild(uploadLabel);
+        this.dropArea.appendChild(extensionSpan);
+        this.dropArea.appendChild(sizeSpan);
+        this.dropArea.appendChild(this.fileSpan);
+        this.dropArea.appendChild(this.errorSpan);
 
-        return dropArea;
+        return this.dropArea;
+    }
+
+    public doValidateAndGetBase64(file: File) {
+        if (!this.validateFile(file.size, Upload.getFileExtensionFromFile(file)))
+            return;
+
+        this.setFileNameToSpan(file.name).setErrorSpanTo("");
+
+        this.getBase64(file)
+            .then(data => {
+                this.dropArea.setAttribute("value", JSON.stringify({
+                    data: data.toString().split(",")[1],
+                    fileExtension: Upload.getFileExtensionFromFile(file),
+                    fileName: file.name,
+                    mimeType: file.type
+                }))
+            })
+            .catch(reason => console.error("ERROR: " + reason));
     }
 
     public getBase64(file: File) {
@@ -121,6 +113,47 @@ export class Upload implements IRenderable {
             reader.onload = () => resolve(reader.result!);
             reader.onerror = error => reject(error);
         });
+    }
+
+    public static getFileExtensionFromFile(file: File) {
+        const array = file.name.split(".");
+        return array[array.length - 1];
+    }
+
+    public setFileNameToSpan(name: string) {
+        this.fileSpan.innerText = name;
+        return this
+    }
+
+    public setErrorSpanTo(text: string) {
+        this.errorSpan.innerText = text;
+        return this
+    }
+
+    public validateFile(size: number, extension?: string): boolean {
+        this.setErrorSpanTo("");
+        this.errorSpan.classList.remove("lto-upload-extension-error", "lto-upload-size-error");
+
+        let extensionAllowed = true;
+        let sizeAllowed = true;
+
+        if (extension && this.spec.accept) {
+            const allowedExtensions = this.spec.accept.replace(/\s/g, '').split(",");
+            extensionAllowed = allowedExtensions.find(e => e === `${extension}`) !== undefined;
+
+            if (!extensionAllowed) {
+                this.setFileNameToSpan("");
+                this.errorSpan.classList.add("lto-upload-extension-error");
+            }
+        }
+
+        if (size > this.spec.maxSize! * 1024 * 1024) {
+            this.setFileNameToSpan("");
+            this.errorSpan.classList.add("lto-upload-size-error");
+            sizeAllowed = false;
+        }
+
+        return sizeAllowed && extensionAllowed
     }
 }
 
