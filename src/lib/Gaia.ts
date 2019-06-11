b768561edf8b80549bcb6139736e1c037e8486dc
Michael Mair <michael.mair@leftshift.one>
2019-06-11 12:41:48 +0200

    enabled optional credentials for connecting to GAIA

--- src/lib/Gaia.ts
+++ src/lib/Gaia.ts
@@ -13,8 +13,7 @@ export class Gaia {
         this.renderer = renderer;
     }
 
-    public connect(url: string, identity: string): Promise<MqttConnection> {
-        return new Promise((resolve) => resolve(new MqttConnection(url, identity, this.renderer, this.listener)));
+    public connect(url: string, identity: string, username: string | null = null, password: string | null = null): Promise<MqttConnection> {
+        return new Promise((resolve) => resolve(new MqttConnection(url, identity, this.renderer, this.listener, username, password)));
     }
-
 }
