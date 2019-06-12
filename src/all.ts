159ab4c81b36a4cc943a0f3a6be3c163f4eb8a0c
M <manuel@weixler.me>
2019-06-12 15:08:09 +0200

    (Packaage)Subpackage "all" contains now all available elements

--- src/all.ts
+++ src/all.ts
@@ -1,1 +1,155 @@
-require("./std");
+import {Gaia} from './lib/Gaia';
+import {ChannelType} from './lib/support/ChannelType';
+import {OffSwitchListener} from './lib/listener/OffSwitchListener';
+import {Defaults} from './lib/support/Defaults';
+import EventStream from "./lib/event/EventStream";
+import {VoiceBehaviour} from "./lib/behaviour/VoiceBehaviour";
+import {WebRTCRecorder} from "./lib/audio/recorder/WebRTCRecorder";
+import {BufferedAudioPlayer} from "./lib/audio/player/BufferedAudioPlayer";
+import {NoopRenderer} from "./lib/renderer/NoopRenderer";
+import {DefaultListener} from "./lib/listener/DefaultListener";
+import {ClassicRenderer} from './lib/renderer/ClassicRenderer';
+import {ContentCentricRenderer} from './lib/renderer/ContentCentricRenderer';
+import {RevealJsRenderer} from './lib/renderer/RevealJsRenderer';
+import {MouseBehaviour} from './lib/behaviour/MouseBehaviour';
+import {KeyboardBehaviour} from './lib/behaviour/KeyboardBehaviour';
+import {Block} from './lib/renderable/block';
+import {Camera} from "./lib/renderable/camera";
+import {Selection} from './lib/renderable/selection';
+import {Bold} from './lib/renderable/bold';
+import {Break} from './lib/renderable/break';
+import {Button} from './lib/renderable/button';
+import {Calendar} from './lib/renderable/calendar';
+import {Carousel} from './lib/renderable/carousel';
+import {Checkbox} from './lib/renderable/checkbox';
+import {Container} from './lib/renderable/container';
+import {Headline} from './lib/renderable/headline';
+import {Image} from './lib/renderable/image';
+import {Item} from './lib/renderable/item';
+import {Items} from './lib/renderable/items';
+import {Link} from './lib/renderable/link';
+import {Reel} from './lib/renderable/reel';
+import {Upload} from './lib/renderable/upload';
+import {Slider} from './lib/renderable/slider';
+import {SlotMachine} from './lib/renderable/slotmachine';
+import {Spinner} from './lib/renderable/spinner';
+import {Form} from "./lib/renderable/form";
+import {Email} from "./lib/renderable/email";
+import {Phone} from "./lib/renderable/phone";
+import {TextInput} from "./lib/renderable/textInput";
+import {Submit} from './lib/renderable/submit';
+import {Suggestion} from './lib/renderable/suggestion';
+import {Table} from './lib/renderable/table';
+import {Col} from "./lib/renderable/table/col";
+import {Row} from "./lib/renderable/table/row";
+import {ReelValue} from "./lib/renderable/reelValue";
+import {Text} from './lib/renderable/text';
+import {CheckboxChoice, MultipleChoice, RadioChoice, SingleChoice} from "./lib/renderable/choice";
+import {Textarea} from "./lib/renderable/textarea";
+import Renderables from "./lib/renderable/Renderables";
+import Properties from "./lib/renderable/Properties";
+import {DatePicker} from './lib/renderable/datePicker';
+import {DateTimePicker} from './lib/renderable/dateTimePicker';
+import {Map} from './lib/renderable/map';
+import {CodeReader} from './lib/renderable/codeReader';
+import {Heatmap} from './lib/renderable/vis/heatmap/Heatmap';
+import {Sunburst} from './lib/renderable/vis/sunburst/Sunburst';
+import {Sankey} from './lib/renderable/vis/sankey/Sankey';
+import Scatterplot from './lib/renderable/vis/scatterplot/Scatterplot';
+import Stackedbar from './lib/renderable/vis/stackedbar/Stackedbar';
+import HeatmapOptions from './lib/renderable/vis/heatmap/HeatmapOptions';
+import SunburstOptions from './lib/renderable/vis/sunburst/SunburstOptions';
+import SankeyOptions from './lib/renderable/vis/sankey/SankeyOptions';
+import ScatterplotOptions from './lib/renderable/vis/scatterplot/ScatterplotOptions';
+import StackedbarOptions from './lib/renderable/vis/stackedbar/StackedbarOptions';
+import {Doughnut} from './lib/renderable/vis/doughnut/Doughnut';
+import DoughnutOptions from './lib/renderable/vis/doughnut/DoughnutOptions';
+import {Line} from './lib/renderable/vis/line/Line';
+import LineOptions from './lib/renderable/vis/line/LineOptions';
+import {Bar} from './lib/renderable/vis/bar/Bar';
+import BarOptions from './lib/renderable/vis/bar/BarOptions';
+import {Graph} from './lib/renderable/vis/graph/Graph';
+import GraphOptions from './lib/renderable/vis/graph/GraphOptions';
+import {Bar3D} from './lib/renderable/vis/bar3d/Bar3D';
+import Bar3DOptions from './lib/renderable/vis/bar3d/Bar3DOptions';
+
+
+export {
+    VoiceBehaviour,
+    WebRTCRecorder,
+    BufferedAudioPlayer,
+    NoopRenderer,
+    DefaultListener,
+    DatePicker,
+    DateTimePicker,
+    CodeReader,
+    Map,
+    ClassicRenderer,
+    ContentCentricRenderer,
+    RevealJsRenderer,
+    Gaia,
+    MouseBehaviour,
+    KeyboardBehaviour,
+    ChannelType,
+    OffSwitchListener,
+    Defaults,
+    Renderables,
+    Properties,
+    EventStream,
+    Block,
+    Bold,
+    Break,
+    Button,
+    Calendar,
+    Upload,
+    Carousel,
+    Checkbox,
+    Container,
+    Headline,
+    Image,
+    Item,
+    Items,
+    Form,
+    Email,
+    Camera,
+    TextInput,
+    Phone,
+    Link,
+    Reel,
+    Slider,
+    SlotMachine,
+    Spinner,
+    Submit,
+    Suggestion,
+    Table,
+    Text,
+    Textarea,
+    Row,
+    Col,
+    Selection,
+    ReelValue,
+    SingleChoice,
+    MultipleChoice,
+    CheckboxChoice,
+    RadioChoice,
+    Heatmap,
+    HeatmapOptions,
+    Sunburst,
+    SunburstOptions,
+    Sankey,
+    SankeyOptions,
+    Scatterplot,
+    ScatterplotOptions,
+    Stackedbar,
+    StackedbarOptions,
+    Doughnut,
+    DoughnutOptions,
+    Line,
+    LineOptions,
+    Bar,
+    BarOptions,
+    Graph,
+    GraphOptions,
+    Bar3D,
+    Bar3DOptions
+};
